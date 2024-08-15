8d6b85f11cd8f0e3ab5d751fa5b63cf22f6588cc
bohyuny <bo3959@gmail.com>
2024-08-15 22:35:33 +0900

    [메모장] delete 추가

--- main.py
+++ main.py
@@ -27,4 +27,12 @@ def put_memo(req_memo:Memo):
             return '성공했어요'
     return '그런 메모는 없어요'
 
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id):
+      for index, memo in enumerate (memos):
+        if memo.id == memo_id:
+            memos.pop(index) 
+            return '성공했어요'
+        return '그런 메모는 없어요'
+
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
--- static/app.js
+++ static/app.js
@@ -12,21 +12,36 @@ async function editMemo(event) {
       content: editInput,
     }),
   });
+  readMemo();
+}
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id;
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE",
+  });
+  readMemo();
 }
 
 function displayMemo(memo) {
   const ul = document.querySelector("#memo-ul");
 
   const li = document.createElement("li");
-  li.innerText = `[id:${memo.id}] ${memo.content}`;
+  li.innerText = `${memo.content}`;
 
   const editBtn = document.createElement("button");
   editBtn.innerText = "수정하기";
   editBtn.addEventListener("click", editMemo);
   editBtn.dataset.id = memo.id;
 
-  ul.appendChild(li);
+  const delBtn = document.createElement("button");
+  delBtn.innerHTML = "삭제";
+  delBtn.addEventListener("click", deleteMemo);
+  delBtn.dataset.id = memo.id;
+
   li.appendChild(editBtn);
+  li.appendChild(delBtn);
+  ul.appendChild(li);
 }
 
 async function readMemo() {
